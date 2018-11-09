496ee8672750aa18303dbf95bb69ac2c2e189c70
Brooks <brooksyang@outlook.com>
2018-11-09 15:27:44 +0800

    Home页面

--- src/router.js
+++ src/router.js
@@ -1,10 +1,15 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Home from './views/Home.vue';
 
 Vue.use(Router);
 
 const routes = [
-
+    {
+        path: '/home',
+        name: 'home',
+        component: Home,
+    }
 ];
 
 export default new Router({
